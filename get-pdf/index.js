abe92c9ad87f5f362bb4ad8dbf00f60b455798a6
Aleksei Fedorenko <fedorenksei@gmail.com>
2023-09-29 11:17:06 +0300

    feat: correct pdf generation script

--- get-pdf/index.js
+++ get-pdf/index.js
@@ -1,10 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: 'new' });
   try {
     const page = await browser.newPage();
-    await page.setViewport({ width: 1080, height: 1024 });
     await main(page);
   } catch (e) {
     console.error(e);
@@ -17,7 +16,5 @@ async function main(page) {
   await page.goto('http://127.0.0.1:5500/src');
   await page.pdf({
     path: 'get-pdf/result.pdf',
-    format: 'A4',
-    margin: { top: 1, right: 1, bottom: 1, left: 1 },
   });
 }
